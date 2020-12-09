ed51cfd22dea3dfd970785971a5a7661cb66ebb4
laisMaas <lais_m_lopes@yahoo.com.br>
2020-12-09 05:38:03 -0300

    gerenciando erros simples no server e debug configurado

--- server.js
+++ server.js
@@ -15,13 +15,18 @@ const router = express.Router();
 const route = router.get('/', (req, res, next) => {
     res.status(200).send({
         title: "Node Store API",
-        version: "Teste"
+        version: "Teste3"
     });
 });
 app.use('/', route);
 
 server.listen(port);
+server.on('error', onError);
+server.on('listening', onListening);
+
 console.log('API rodando na porta ' + port);
+
+
 
 function normalizePort(val) {
     const port = parseInt(val, 10);
@@ -35,3 +40,35 @@ function normalizePort(val) {
     return false;
 }
 
+
+function onError(error) {
+    if (error.syscall !== 'listen'){
+        throw error;
+    }
+
+    const bind = typeof port === 'string' ?
+        'Pipe' + port :
+        'Port' + port;
+    
+    switch (error, code) {
+        case 'EACCES':
+            console.error(bind + 'requires elevated privileges');
+            process.exit(1);
+            break;
+        case 'EADDRINUSE':
+            console.error(bind + 'is already in use');
+            process.exit(1);
+            break;
+        default:
+            throw error;
+    }  
+}
+
+
+function onListening() {
+    const addr = server.address();
+    const bind = typeof addr === 'string'
+        ? 'pipe' + addr
+        : 'port' + addr.port;
+    debug('Listening on' + bind);
+}
