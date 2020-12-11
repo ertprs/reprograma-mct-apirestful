fe97ac79a288b4e6104cd694dbf5f67be7780c94
laisMaas <lais_m_lopes@yahoo.com.br>
2020-12-11 04:58:39 -0300

    iniciando o trabalho na controllers

---
+++ src/models/empresa.js
@@ -0,0 +1,9 @@
+/*const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const schema = new Schema({
+
+    ******colocar aqui o qu irá compor a empresa*****
+});
+
+module.exports = mongoose.model('empresa', schema);*/
---
+++ src/routes/index-route.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const express = require('express');
+const router = express.Router();
+
+
+router.get('/', (req, res, next) => {
+    res.status(200).send({
+        title: "Node Store API MCT",
+        version: "Teste7"
+    });
+});
+
+module.exports = router;
